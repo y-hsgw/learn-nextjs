ba1e6a0dccc62b6215c62d97237565fe49be3cfc
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-11-13 20:22:59 +0900

    画像の最適化

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import styles from "@/app/ui/home.module.css";
 import { lusitana } from "@/app/ui/fonts";
 
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Page() {
   return (
@@ -32,6 +33,20 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop and mobile versions"
+          />
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshots of the dashboard project mobile versions"
+          />
         </div>
       </div>
     </main>
