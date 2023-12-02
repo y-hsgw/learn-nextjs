812f15ca951c60ab4251aea7cf164645873a4d86
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-12-02 20:59:21 +0900

    コメント復活して遅延処理しておく

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -19,12 +19,13 @@ export async function fetchRevenue() {
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in real life :)
-    // console.log("Fetching revenue data...");
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+
+    console.log("Fetching revenue data...");
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    // console.log("Data fetch complete after 3 seconds.");
+    console.log("Data fetch complete after 3 seconds.");
 
     return data.rows;
   } catch (error) {
